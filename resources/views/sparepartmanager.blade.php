005a85d06a4066c76f2eb1f43bbabf44c1124d87
Mukhriddin0555 <romantik4350555@gmail.com>
2022-01-12 12:49:29 +0500

    new version

---
+++ resources/views/sparepartmanager.blade.php
@@ -0,0 +1,7 @@
+<x-transfer.all-transfer>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            Добро пожаловать
+        </h2>
+    </x-slot>
+</x-transfer.all-transfer>
